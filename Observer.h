6ff621b1b0315288619ebd671ba130494f5f8fb2
lexlam1524 <lamkachun1524@gmail.com>
2024-10-10 20:54:50 +1100

    Revert "fix conflicts"
    This reverts commit 4143d8be898dfb5a72cccdb881ca4ff927bb5378.

--- Observer.h
+++ Observer.h
@@ -1,12 +1,14 @@
 // observer.h
 #ifndef OBSERVER_H
 #define OBSERVER_H
+#include "obstacle.h"
+
+class Obstacle;
 
 class Observer {
 public:
     virtual ~Observer() {}
     virtual void CollisionUpdate(bool is_collision)=0;
-    virtual void deceaseSpeed(int newSpeed)=0;
 };
 
 #endif  // OBSERVER_H
--- globals.h
+++ globals.h
@@ -4,13 +4,10 @@
 extern bitmap background;
 extern bitmap bee;
 extern bitmap box;
-extern bitmap bullet;  // Keep your bullet bitmap
+extern bitmap bullet;
 extern float player_posx;
 extern float player_posy;
-extern int RIGHT_BOUNDARY;
-extern int LEFT_BOUNDARY;
+extern int RIGHT_BOUNDARY ;
+extern int LEFT_BOUNDARY ;
 extern int GRAVITY;
-extern int WINDOW_WIDTH;  // Add from upstream
-extern int WINDOW_HEIGHT; // Add from upstream
 extern int spawn_interval;
-extern float BEE_SCALE;   // Add from upstream
